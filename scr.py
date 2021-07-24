da1a5dfe5bf6dfb4a3f1eeacb234ce8c1926adc2
kojimakentaro <kentaroukojiken@gmail.com>
2021-07-25 00:32:52 +0900

    main

--- scr.py
+++ scr.py
@@ -22,14 +22,14 @@ driver.implicitly_wait(10)
 
 
 #driver.getを使用し、サイトにアクセスを行う。(sleepを挟んで負荷をかけすぎないようにする。)
-driver.get("https://www.google.com")
+driver.get("https://www.yahoo.co.jp")
 sleep(2)
 
-#サイトまでたどり、クリック検索を行う
-#a_tag = driver.find_element_by_css_selector("div._2jjSS8r_I9Zd6O9NFJtD > ul > li:nth-of-type(2) > a")
-#sleep(3)
 
-#a_tag.click()
+#スクロール作業を行う
+#height = driver.execute_script("return document.body.scrollHeight")
+#sleep(3)
+#driver.execute_script(f"window.scrollTo(0,{height})") #(x,y")で記入を行う
 #sleep(3)
 
 
@@ -41,13 +41,33 @@ sleep(2)
 #print(a.text)
 
 #検索タブに関しての操作(文字を入力を行う)
-#search_box = driver.find_element_by_css_selector("input.nxrKmn7Pk9iZExZ0kdJ4Z")
-#sleep(2)
-#search_box.send_keys("オリンピック")
-#sleep(5)
+search_box = driver.find_element_by_css_selector("input.nxrKmn7Pk9iZExZ0kdJ4Z")
+sleep(2)
+search_box.send_keys("オリンピック")
+sleep(5)
 #実際に検索ボタンを押す
-#search_box.submit()
+search_box.submit()
+sleep(3)
 
+#最下点まで移動する
+while True:
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    sleep(2)
+
+    #次のページのボタンを押す
+    a = driver.find_elements_by_css_selector("div..Contents__inner.Co > div > div > div > div > a")
+    sleep(3)
+
+    #ページ移動のボタンを押す
+    if a:
+        a[0].click()
+    else:
+        break
+#スクロール作業を行う
+#height = driver.execute_script("return document.body.scrollHeight")
+#sleep(3)
+#driver.execute_script(f"window.scrollTo(0,{height})") #(x,y")で記入を行う
+#sleep(3)
 
 
 #HTMLタグを指定して、そこにある情報を抽出する
