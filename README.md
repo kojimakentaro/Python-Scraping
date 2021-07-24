10455e4255bfc73b9a6e1e0283dfc3fdfcf5ccbd
kojima kentaro <kentaroukojiken@gmail.com>
2021-07-24 17:39:55 +0900

    main

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Python-Scraping
+Pythonを使用したスクレイピング用のリポジトリーです。
+Googleなどを使い、データを取得します。
+
---
+++ scr.py
@@ -0,0 +1,30 @@
+#プラグインのインポートを行う
+from typing import Text
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+#ヘッドレスモード(使用することにより、実際のサイトの表示はなくなる)
+#options.add_argument("--headless")
+#シークレットモード
+options.add_argument("--incognito")
+
+
+#driverを作成し、chromeを使用できるようにする
+driver = webdriver.Chrome(
+    executable_path = "/Users/kojimakentaro/Python-Scraping/chromedriver",options = options
+)
+
+#driver.getを使用し、サイトにアクセスを行う。(sleepを挟んで負荷をかけすぎないようにする。)
+driver.get("https://www.google.co.jp")
+sleep(3)
+
+#要素から抜ける
+driver.quit()
+
+
+
+
+
