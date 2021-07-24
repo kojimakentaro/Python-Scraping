77527b85d265ff6627a5a9e583d05769b2b37527
kojimakentaro <kentaroukojiken@gmail.com>
2021-07-24 22:06:21 +0900

    main

--- scr.py
+++ scr.py
@@ -7,9 +7,9 @@ from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 #ヘッドレスモード(使用することにより、実際のサイトの表示はなくなる)
-#options.add_argument("--headless")
+options.add_argument("--headless")
 #シークレットモード
-options.add_argument("--incognito")
+#options.add_argument("--incognito")
 
 
 #driverを作成し、chromeを使用できるようにする
@@ -18,8 +18,21 @@ driver = webdriver.Chrome(
 )
 
 #driver.getを使用し、サイトにアクセスを行う。(sleepを挟んで負荷をかけすぎないようにする。)
-driver.get("https://www.google.co.jp")
+driver.get("https://www.yahoo.co.jp")
 sleep(2)
+
+i = driver.find_element_by_id("Topics")
+
+print(i)
+print(i.text)
+
+#HTMLタグを指定して、そこにある情報を抽出する
+#h2_tags = driver.find_elements_by_tag_name("h2")
+
+#for h2_tag in h2_tags:
+    #print(h2_tag.text)
+    #print(h2_tag.get_attribute("outHTML"))
+
 
 #要素から抜ける
 driver.quit()
